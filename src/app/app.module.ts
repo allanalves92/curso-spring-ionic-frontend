6d871aaf4f5cf5bbdc481ed0b3cdfadb43640885
Allan Alves <allanalvesmail@gmail.com>
2018-11-11 19:26:22 -0200

    Criando um interceptor para tratamento de erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MyApp } from "./app.component";
 
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+import { ErrorInterceptorProvider } from "../interceptors/error-interceptor";
 
 @NgModule({
   declarations: [MyApp],
@@ -18,7 +19,8 @@ import { SplashScreen } from "@ionic-native/splash-screen";
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
-    CategoriaService
+    CategoriaService,
+    ErrorInterceptorProvider
   ]
 })
 export class AppModule {}
